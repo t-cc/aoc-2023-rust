207df5984c38c22836cc6ff0ffe823e96f53b47c
Toño <joseantonio@z1.digital>
2023-12-01 17:06:37 +0100

    feat: day 01 part b

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,9 @@
 advent_of_code::solution!(1);
 
+const NUMBER_NAMES: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
 pub fn part_one(input: &str) -> Option<u32> {
     let mut sum = 0;
     for line in input.lines() {
@@ -9,15 +13,43 @@ pub fn part_one(input: &str) -> Option<u32> {
                 numbers.push(c.to_digit(10).unwrap());
             }
         }
-        let first = numbers.first().copied().unwrap();
-        let last = numbers.last().copied().unwrap();
-        sum += first * 10 + last;
+        if numbers.len() > 0 {
+            let first = numbers.first().copied().unwrap();
+            let last = numbers.last().copied().unwrap();
+            sum += first * 10 + last;
+        }
     }
     return sum.into()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut sum = 0;
+    for line in input.lines() {
+        let mut numbers = Vec::new();
+        let mut index = 0;
+        let line_size= line.len();
+        while index < line_size {
+            let slice = &line[index..line_size ];
+            let char = slice.to_string().chars().next().unwrap();
+            if char.is_ascii_digit() {
+                numbers.push(char.to_digit(10).unwrap());
+            } else {
+                for (name_index, name) in NUMBER_NAMES.iter().enumerate() {
+                    if slice.starts_with(name) {
+                        numbers.push(name_index as u32 + 1);
+                        break
+                    }
+                }
+            }
+            index += 1
+        }
+        let first = numbers.first().copied().unwrap();
+        let last = numbers.last().copied().unwrap();
+        println!("{}", line);
+        println!("{}", (first * 10 + last));
+        sum += first * 10 + last;
+    }
+    return sum.into()
 }
 
 #[cfg(test)]
@@ -27,12 +59,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(209u32));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(282u32));
     }
 }
