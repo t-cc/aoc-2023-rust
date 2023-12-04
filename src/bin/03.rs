a3400ca34ac5ff27567f5a8ba9abf427c1113be8
Toño <tonio.castro@gmail.com>
2023-12-04 22:31:18 +0100

    feat: day 03 part b

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -84,8 +84,94 @@ pub fn part_one(input: &str) -> Option<u32> {
     return (sum as u32).into();
 }
 
+struct NumberPosition {
+    value: i32,
+    start: usize,
+    end: usize,
+}
+
+fn get_numbers_from_line(rows: &str, numbers: &mut Vec<NumberPosition>) {
+    let mut found_a_number_start = false;
+    let mut where_number_starts = 0usize;
+    let mut number = String::new();
+    for (i, char) in rows.chars().enumerate() {
+        if char.is_ascii_digit() {
+            if !found_a_number_start {
+                found_a_number_start = true;
+                where_number_starts = i;
+            }
+            number.push(char);
+        } else {
+            found_a_number_start = false;
+            if number.len() > 0 {
+                let mut number_data = NumberPosition {
+                    value: number.parse::<i32>().unwrap(),
+                    start: where_number_starts,
+                    end: where_number_starts + number.len(),
+                };
+                if number_data.start > 0 {
+                    number_data.start = number_data.start - 1;
+                }
+                numbers.push(number_data);
+                println!("{:?}", number)
+            }
+            number = String::new();
+        }
+    }
+    if number.len() > 0 {
+        let mut number_data = NumberPosition {
+            value: number.parse::<i32>().unwrap(),
+            start: where_number_starts,
+            end: where_number_starts + number.len(),
+        };
+        if number_data.start > 0 {
+            number_data.start = number_data.start - 1;
+        }
+        numbers.push(number_data);
+        println!("{:?}", number)
+    }
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut sum = 0i32;
+
+    let mut index = 0usize;
+    let rows: Vec<&str> = input.lines().collect::<Vec<_>>();
+
+    while index < rows.len() {
+        let mut numbers: Vec<NumberPosition> = Vec::new();
+        // numbers in the current row
+        get_numbers_from_line(rows[index], &mut numbers);
+        // from the previous line...
+        if index > 0 {
+            get_numbers_from_line(rows[index - 1], &mut numbers);
+        }
+        // and the next line
+        if index + 1 < rows.len() {
+            get_numbers_from_line(rows[index + 1], &mut numbers);
+        }
+
+        // find * in current line
+        for (i, char) in rows[index].chars().enumerate() {
+            if char == '*' {
+                let mut num_list: Vec<i32> = Vec::new();
+                for number in &numbers {
+                    if number.start <= i && number.end >= i {
+                        num_list.push(number.value)
+                    }
+                }
+                if num_list.len() == 2 {
+                    let ratio = num_list.iter().next().unwrap() * num_list.iter().last().unwrap();
+                    sum += ratio;
+                    println!("{:?} -> {:?}", num_list, ratio);
+                }
+            }
+        }
+
+        index += 1;
+    }
+
+    return (sum as u32).into();
 }
 
 #[cfg(test)]
@@ -95,12 +181,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(4360u32));
+        assert_eq!(result, Some(4361u32));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835u32));
     }
 }
