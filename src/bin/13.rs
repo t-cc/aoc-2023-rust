38e70f197363c035d924b1a919f04c35f54fae37
Toño <tonio.castro@gmail.com>
2024-01-06 13:01:39 +0100

    feat: day 13 part b (test only)

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,7 +1,6 @@
 advent_of_code::solution!(13);
 
 fn find_first_vertical_reflection(pattern: Vec<String>) -> u32 {
-    let mut sum = 0u32;
     let max_y = pattern.len() - 1;
     for i in 0..max_y {
         // println!("INDEX {:?}", i);
@@ -14,11 +13,11 @@ fn find_first_vertical_reflection(pattern: Vec<String>) -> u32 {
             }
         }
         if all_equal {
-            sum += (i + 1) as u32;
+            return (i + 1) as u32;
         }
     }
 
-    return sum;
+    return 0u32;
 }
 
 fn transpose_patter(pattern: Vec<String>) -> Vec<String> {
@@ -35,8 +34,7 @@ fn transpose_patter(pattern: Vec<String>) -> Vec<String> {
     return transposed;
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut sum = 0u32;
+fn build_patterns(input: &str) -> Vec<Vec<String>> {
     let mut pattern: Vec<String> = Vec::new();
     let mut pattern_list: Vec<Vec<String>> = Vec::new();
 
@@ -52,29 +50,87 @@ pub fn part_one(input: &str) -> Option<u32> {
     }
     if !pattern.is_empty() {
         pattern_list.push(pattern);
-        pattern = Vec::new();
     }
+    return pattern_list;
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut sum = 0u32;
+
+    let pattern_list = build_patterns(input);
     // println!("patterns: {:?}", pattern_list);
 
     for pattern in pattern_list {
         let vertical = find_first_vertical_reflection(pattern.clone());
         // println!("{:?} -> vertical {:?}", pattern, vertical);
-        if vertical > 0 {
-            sum += 100 * vertical;
-        }
+
+        sum += 100 * vertical;
+
         let transposed = transpose_patter(pattern.clone());
         let horizontal = find_first_vertical_reflection(transposed.clone());
         // println!("{:?} -> horizontal {:?}", transposed, horizontal);
-        if horizontal > 0 {
-            sum += horizontal;
-        }
+        sum += horizontal;
     }
 
     return Some(sum);
 }
 
+fn find_different_reflection(pattern: Vec<String>, original: u32) -> u32 {
+    let first: String = pattern.first().unwrap().clone();
+
+    let y_max = pattern.len();
+    let x_max = first.len();
+    for y in 0..y_max {
+        for x in 0..x_max {
+            let mut new_pattern = pattern.clone();
+
+            let mut row = pattern.iter().nth(y).unwrap().clone();
+            // if y == 0 {
+            //     println!("ORIGNAL ROW {:?}", row);
+            // }
+            let char = row.chars().nth(x).unwrap();
+            let mut next_chart = ".";
+            if char == '.' {
+                next_chart = "#";
+            }
+            row.replace_range(x..x + 1, next_chart);
+            // if y == 0 {
+            //    println!("NEW ROW     {:?}", row);
+            // }
+            new_pattern[y] = row;
+            let new_reflection = find_first_vertical_reflection(new_pattern.clone());
+            // println!(
+            //    "CLEANED: {:?} -> {:?} ?= {:?}",
+            //    new_pattern, new_reflection, original
+            // );
+            if new_reflection > 0 && new_reflection != original {
+                return new_reflection;
+            }
+        }
+    }
+
+    return 0u32;
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut sum = 0u32;
+    let pattern_list = build_patterns(input);
+    // println!("patterns: {:?}", pattern_list);
+
+    for pattern in pattern_list {
+        let vertical = find_first_vertical_reflection(pattern.clone());
+        // println!("{:?} -> vertical {:?}", pattern, vertical);
+        let new_vertical = find_different_reflection(pattern.clone(), vertical);
+        sum += 100 * new_vertical;
+
+        let transposed = transpose_patter(pattern.clone());
+        let horizontal = find_first_vertical_reflection(transposed.clone());
+        // println!("{:?} -> horizontal {:?}", transposed, horizontal);
+        let new_horizontal = find_different_reflection(transposed.clone(), horizontal);
+        sum += new_horizontal;
+    }
+
+    return Some(sum);
 }
 
 #[cfg(test)]
@@ -84,12 +140,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(4059u32));
+        assert_eq!(result, Some(405u32));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(400u32));
     }
 }
