93025e3512278c1b4399208bc0afac5de0b84797
Toño <joseantonio@z1.digital>
2024-01-10 17:52:37 +0100

    feat: day 15 part b

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -22,8 +22,65 @@ pub fn part_one(input: &str) -> Option<u32> {
     return Some(sum);
 }
 
+#[derive(Debug, Clone)]
+struct Lens {
+    label: String,
+    focal: u32,
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut boxes : Vec<Vec<Lens>> = vec![vec![]; 256];
+
+    let line: Vec<&str> = input.lines().next().unwrap().split(',').collect();
+    for part in line {
+        let has_equal = part.chars().find(|&c| c == '=').is_some();
+        let mut info = Vec::new();
+        let mut focal_length = 0u32;
+        if has_equal {
+            info = part.split('=').collect();
+            focal_length = info.last()?.parse::<u32>().unwrap();
+        } else {
+            info = part.split('-').collect();
+        }
+        let label_str = info.first().unwrap();
+        let box_number = get_value(label_str);
+        let label = label_str.to_string();
+        // println!("Label {:?} -> value {:?}, focus {:?}", label, box_number, focal_length);
+
+
+        let existing = boxes[box_number as usize].to_vec();
+        let lens_index =  existing.iter().position(|l| l.label == label);
+        if has_equal {
+            if lens_index.is_some() {
+                // replace focal
+                boxes[box_number as usize][lens_index.unwrap()].focal = focal_length;
+
+            } else {
+                // append
+                boxes[box_number as usize].push(Lens {
+                    label: label.to_string(),
+                    focal: focal_length
+                })
+            }
+        } else if lens_index.is_some() {
+            // remove the relevant lens in the current box
+            boxes[box_number as usize] = existing.iter().filter(|l| l.label != label ).cloned().collect();
+        }
+        // println!("BOXES: {:?}", boxes);
+    }
+
+
+    let mut sum = 0u32;
+
+    for i in 0..boxes.len() {
+        let box_count = (i + 1) as u32;
+        for j in 0..boxes[i].len() {
+            let slot_count = (j +1) as u32;
+            sum += box_count * slot_count * boxes[i][j].focal;
+        }
+
+    }
+    return Some(sum);
 }
 
 #[cfg(test)]
@@ -39,6 +96,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(145u32));
     }
 }
